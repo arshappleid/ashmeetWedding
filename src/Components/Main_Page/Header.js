2fb450bf2cdf8eba93b4b6c768143da08e4404ff
Prabhmeet Singh Deol <deol@prabhmeets-air.myfiosgateway.com>
2022-01-12 19:56:35 -0500

    fixed top header

--- src/Components/Main_Page/Header.js
+++ src/Components/Main_Page/Header.js
@@ -4,14 +4,15 @@ class Header extends Component {
 	render() {
 		return (
 			<div class="topheader " id="topHeader">
-				<div class = "flex space-x-4">
+
 					<img src = "./../images/wedding.jpg" className="rounded-full w-16 h-32"/>
 					<h2>Ashmeet Gill weds Parampal Sandhu</h2>
-					<img src = "./../images/wedding.jpg" className="rounded-full w-16 h-32"/>
-				</div>
+					<img src="./../images/wedding.jpg" className="rounded-full w-16 h-32" />
+
 			</div>
+
 		)
 	}
 }
-
+ 
 export default Header
